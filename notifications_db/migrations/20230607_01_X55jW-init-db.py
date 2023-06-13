21056d95752ad90e6127ebeaca11caaa67068813
vgaksentii <vgaksentii@domclick.ru>
2023-06-13 10:39:19 +0300

    поправила Dockerfile, добавила миграции

--- notifications_db/migrations/20230607_01_X55jW-init-db.py
+++ notifications_db/migrations/20230607_01_X55jW-init-db.py
@@ -7,4 +7,33 @@ from yoyo import step
 
 __depends__ = {}
 
-steps = [step("")]
+steps = [
+    step(
+        """
+        SET statement_timeout TO '2s';
+        CREATE TABLE IF NOT EXISTS notifications (
+            notification_id serial4 NOT NULL PRIMARY KEY,
+            template_id text NOT NULL,
+            recipient_id text NULL,
+            excluded boolean DEFAULT FALSE,
+            exclude_reason varchar NULL,
+            created_dt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_dt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+    
+        CREATE TABLE IF NOT EXISTS notifications_distributions (
+            id serial4 NOT NULL PRIMARY KEY,
+            notification_id int4 NOT NULL,
+            recipient_id text NULL,
+            status varchar NOT NULL,
+            created_dt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_dt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        """,
+        """
+        SET statement_timeout TO '2s';
+        DROP TABLE IF EXISTS notifications;
+        DROP TABLE IF EXISTS notifications_distributions;
+        """
+    )
+]
