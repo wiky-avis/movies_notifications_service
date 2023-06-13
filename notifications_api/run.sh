75c241dfc42c62c0a98a73cc1710273ba541fe3a
vgaksentii <vgaksentii@domclick.ru>
2023-06-13 09:18:12 +0300

    поправила запуск

--- notifications_api/run.sh
+++ notifications_api/run.sh
@@ -4,4 +4,4 @@ set -e
 
 # Start server
 echo "Starting server"
-gunicorn --worker-class=uvicorn.workers.UvicornWorker --workers=4 -b ${PROJECT_HOST}:${PROJECT_PORT} src.app:app
+gunicorn --worker-class=uvicorn.workers.UvicornWorker --workers=4 -b ${PROJECT_HOST}:${PROJECT_PORT} notifications_api.src.app:app
