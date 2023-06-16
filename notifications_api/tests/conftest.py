579fdb442db4974ab90f9ad8bd54dbdf2ea7563d
vgaksentii <vgaksentii@domclick.ru>
2023-06-16 08:42:06 +0300

    поправила фикстуры

--- notifications_api/tests/conftest.py
+++ notifications_api/tests/conftest.py
@@ -1,20 +1,10 @@
 import pytest_asyncio
 from httpx import AsyncClient
 
-from notifications_api.src.app import create_app
+from notifications_api.src.app import app
 
 
 @pytest_asyncio.fixture
-async def test_app():
-    app = create_app()
-    await app.router.startup()
-    try:
-        yield app
-    finally:
-        await app.router.shutdown()
-
-
-@pytest_asyncio.fixture
-async def test_client(test_app):
-    async with AsyncClient(app=test_app, base_url="http://test") as client:
+async def test_client():
+    async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
