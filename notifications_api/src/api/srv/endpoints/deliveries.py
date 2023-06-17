c781a031f59b57de0818e9e9da79c347996b99f3
vgaksentii <vgaksentii@domclick.ru>
2023-06-17 10:01:17 +0300

    добавила модель ответа

--- notifications_api/src/api/srv/endpoints/deliveries.py
+++ notifications_api/src/api/srv/endpoints/deliveries.py
@@ -4,10 +4,8 @@ from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Body, Depends, Header, HTTPException
 from starlette import status
 
-from notifications_api.src.api.models.delivery import (
-    DeliveryModel,
-    DeliveryResponse,
-)
+from notifications_api.src.api.models.delivery import DeliveryModel
+from notifications_api.src.common.responses import ApiResponse, wrap_response
 from notifications_api.src.common.services.notifications import (
     NotificationsService,
 )
@@ -19,8 +17,9 @@ from notifications_api.src.settings.token import NOTIFICATIONS_SRV_TOKENS
 router = APIRouter()
 
 
-@router.post("/v1/deliveries", response_model=DeliveryResponse)
+@router.post("/v1/deliveries", response_model=ApiResponse)
 @inject
+@wrap_response
 async def create_delivery(
     token_header: Optional[str] = Header(
         None, alias=token_settings.token_header
@@ -38,8 +37,9 @@ async def create_delivery(
     return await notifications_service.create_delivery(body)
 
 
-@router.get("/v1/deliveries/{delivery_id}", response_model=DeliveryResponse)
+@router.get("/v1/deliveries/{delivery_id}", response_model=ApiResponse)
 @inject
+@wrap_response
 async def get_delivery(
     delivery_id: int,
     token_header: Optional[str] = Header(
--- notifications_api/src/app.py
+++ notifications_api/src/app.py
@@ -2,12 +2,17 @@ import sys
 
 import uvicorn as uvicorn
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException, RequestValidationError
 
 from notifications_api.src.api import srv
 from notifications_api.src.common.connectors.amqp import (
     AMQPSenderPikaConnector,
 )
 from notifications_api.src.common.connectors.db import DbConnector
+from notifications_api.src.common.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
 from notifications_api.src.containers import Container
 from notifications_api.src.settings import logger, settings
 
@@ -25,9 +30,13 @@ def create_app() -> FastAPI:
             DbConnector.disconnect,
             AMQPSenderPikaConnector.close,
         ],
+        exception_handlers={
+            HTTPException: http_exception_handler,
+            RequestValidationError: request_validation_exception_handler,
+        },
         title="test",
         openapi_url="/openapi.json",
-        docs_url="/swagger",
+        docs_url="/api/swagger",
         openapi_prefix="",
     )
     app.container = container  # type: ignore
