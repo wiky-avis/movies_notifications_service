fbd4905b014ebd6c2c404f7f5c90e3ea61ec98a6
vgaksentii <vgaksentii@domclick.ru>
2023-06-17 09:25:26 +0300

    добавила таблицу unsubscribed_users

---
+++ notifications_db/migrations/20230617_02_pXSEo-add-table-unsubscribed-users.py
@@ -0,0 +1,25 @@
+"""
+add table unsubscribed_users
+"""
+
+from yoyo import step
+
+__depends__ = {'20230617_01_zyHE2-add-column-errors'}
+
+steps = [
+    step(
+        """
+        SET statement_timeout TO '2s';   
+        CREATE TABLE IF NOT EXISTS unsubscribed_users (
+            id bigserial PRIMARY KEY,
+            user_id bigint NOT NULL,
+            reason text,
+            created_dt timestamp with time zone default now()
+        );
+        """,
+        """
+        SET statement_timeout TO '2s';
+        DROP TABLE IF EXISTS unsubscribed_users;
+        """
+    )
+]
