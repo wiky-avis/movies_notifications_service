5bd1a714ca085dfdde266503cca83ada571439df
opodryadov <oleg_podryadov@mail.ru>
2023-06-20 18:57:48 +0500

    Добавил команду запуска демонов

--- notification_controller/manage.py
+++ notification_controller/manage.py
@@ -20,5 +20,18 @@ def run_consumers(ctx):
         loop.run_forever()
 
 
+@cli.command(help="run-daemons")
+@click.pass_context
+def run_daemons(ctx):
+    from src.workers.cron.runner import main
+
+    with aiomisc.entrypoint(
+        *main.daemons,
+        log_config=False,
+        debug=main.config.debug,
+    ) as loop:
+        loop.run_forever()
+
+
 if __name__ == "__main__":
     cli()
