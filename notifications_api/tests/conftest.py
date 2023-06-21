2e3b1b74eafce7eb047e1dd0e96d1f577be062ae
vgaksentii <vgaksentii@domclick.ru>
2023-06-21 21:46:29 +0300

    добавила фикстуры

--- notifications_api/tests/conftest.py
+++ notifications_api/tests/conftest.py
@@ -1,9 +1,65 @@
+import asyncpg
+import psycopg2
+import pytest
 import pytest_asyncio
 from httpx import AsyncClient
-from src.app import app
+import subprocess
+from urllib.parse import urlsplit
+
+from src.app import create_app
+from src.common.connectors.db import register_json
+
+DATABASE_URL = "postgresql://app:123qwe@localhost:6666/notifications"
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+def migrations():
+    assert urlsplit(DATABASE_URL).path == "/notifications"
+    subprocess.run(
+        [
+            "yoyo",
+            "apply",
+            "--no-config-file",
+            "--database",
+            DATABASE_URL,
+            "../notifications_db/migrations",
+            "-b",
+        ],
+        capture_output=True,
+        check=True,
+    )
 
 
 @pytest_asyncio.fixture
-async def test_client():
-    async with AsyncClient(app=app, base_url="http://test") as client:
+async def test_app(migrations):
+    app = create_app()
+    yield app
+
+
+@pytest_asyncio.fixture
+async def test_client(test_app):
+    async with AsyncClient(app=test_app, base_url="http://test") as client:
         yield client
+
+
+@pytest.fixture
+def clean_table(request):
+    def teardown():
+        clean_tables(*request.param)
+
+    request.addfinalizer(teardown)
+
+
+def clean_tables(*tables):
+    conn = psycopg2.connect(DATABASE_URL)
+    for table in tables:
+        with conn.cursor() as cur:
+            cur.execute("DELETE FROM %s" % table)
+    conn.commit()
+
+
+@pytest_asyncio.fixture
+async def pool():
+    pool = await asyncpg.create_pool(DATABASE_URL, init=register_json)
+    yield pool
+    await pool.close()
